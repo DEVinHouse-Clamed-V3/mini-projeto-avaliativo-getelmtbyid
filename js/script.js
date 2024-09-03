e261161119a8be4784cf9d98263473ceab6b11c0
Gustavo Branquinho <gustavobranquinho2@gmail.com>
2024-09-02 21:03:00 -0300

    feat: Add filter functionality for product search by name or manufacturer

--- js/script.js
+++ js/script.js
@@ -1,130 +1,118 @@
-
-
 import produtosIniciais from "./produtos.json" with {type: "json"}
 
 // Cadastro do produto
+function cadastroFormulario(event) {
+  event.preventDefault();
+  // pegando variaveis
+  const nome = document.getElementById("nome-form").value;
+  const categoria = document.getElementById("categoria-form").value;
+  // const categoria  = document.getElementById('categoria-form')
+  const fabricante = document.getElementById("fabricante-form").value;
+  const preco = document.getElementById("preco-form").value;
+  const quantidade = document.getElementById("estoque-form").value;
+  const foto = document.getElementById("foto-form").value;
+  const id = document.getElementById("id-form").value;
 
-function cadastro_formulario(event) {
-    event.preventDefault()
-    //pegando variaveis
-    const nome = document.getElementById('nome-form').value
-    const categoria = document.getElementById('categoria-form').value;
-    //const categoria  = document.getElementById('categoria-form')
-    const fabricante = document.getElementById('fabricante-form').value
-    const preco = document.getElementById('preco-form').value
-    const quantidade = document.getElementById('estoque-form').value
-    const foto = document.getElementById('foto-form').value
-    const id = document.getElementById('id-form').value
+  // verificação dos campos, prepare-se para muitas linhas!!
+  if (nome === "") {
+    document.getElementById("nome-form").style.borderColor = "red";
+    alert("O nome do produto não foi informado!");
+  } else {
+    document.getElementById("nome-form").style.borderColor = "";
+  }
 
-    // verificação dos campos, prepare-se para muitas linhas!!
-    if (nome === "") {
-        document.getElementById('nome-form').style.borderColor = 'red'
-        alert('O nome do produto não foi informado!')
+  if (categoria === "") {
+    document.getElementById("categoria-form").style.borderColor = "red";
+    alert("A categoria do produto não foi informada!");
+  } else {
+    document.getElementById("categoria-form").style.borderColor = "";
+  }
+
+  if (fabricante === "") {
+    document.getElementById("fabricante-form").style.borderColor = "red";
+    alert("O fabricante do produto não foi informado!");
+  } else {
+    document.getElementById("fabricante-form").style.borderColor = "";
+  }
+
+  if (preco === "") {
+    document.getElementById("preco-form").style.borderColor = "red";
+    alert("O preco do produto não foi informado!");
+  } else {
+    document.getElementById("preco-form").style.borderColor = "";
+  }
+
+  if (quantidade === "") {
+    document.getElementById("estoque-form").style.borderColor = "red";
+    alert("A quantidade do produto não foi informada!");
+  } else {
+    document.getElementById("estoque-form").style.borderColor = "";
+  }
+
+  if (foto === "") {
+    document.getElementById("foto-form").style.borderColor = "red";
+    alert("A foto do produto não foi informada!");
+  } else {
+    document.getElementById("foto-form").style.borderColor = "";
+  }
+
+  //checagem para vericar se pode as informações sao validas para salvar no local storage
+
+  if (nome !== "" && categoria !== "" && fabricante !== "" && preco !== "" && quantidade !== "" && foto !== "") {
+    const produto = {
+      id: Date.now(),
+      nome: nome,
+      categoria: categoria,
+      fabricante: fabricante,
+      preco: preco,
+      quantidade: quantidade,
+      foto: foto,
+    };
+
+    let lista = JSON.parse(localStorage.getItem("produtos")) || [];
+
+    //se tem o id, edita o produto. se nao, cadastra um novo.
+    if (id !== "") {
+      const posicaoProdutoNoArray = lista.findIndex((produto) => produto.id === parseInt(id));
+      lista[posicaoProdutoNoArray] = produto;
+      document.getElementById("id-form").value = "";
     } else {
-        document.getElementById('nome-form').style.borderColor = ''
+      lista.push(produto);
     }
 
-    if (categoria === "") {
-        document.getElementById('categoria-form').style.borderColor = 'red'
-        alert('A categoria do produto não foi informada!')
-    } else {
-        document.getElementById('categoria-form').style.borderColor = ''
-    }
+    document.getElementById("img-form").setAttribute("src", "/image/img-med-generico.png");
 
-    if (fabricante === "") {
-        document.getElementById('fabricante-form').style.borderColor = 'red'
-        alert('O fabricante do produto não foi informado!')
-    } else {
-        document.getElementById('fabricante-form').style.borderColor = ''
-    }
+    localStorage.setItem("produtos", JSON.stringify(lista));
+  }
 
-    if (preco === "") {
-        document.getElementById('preco-form').style.borderColor = 'red'
-        alert('O preco do produto não foi informado!')
-    } else {
-        document.getElementById('preco-form').style.borderColor = ''
-    }
+  document.getElementById("Formulario").reset();
 
-    if (quantidade === "") {
-        document.getElementById('estoque-form').style.borderColor = 'red'
-        alert('A quantidade do produto não foi informada!')
-    } else {
-        document.getElementById('estoque-form').style.borderColor = ''
-    }
-
-    if (foto === "") {
-        document.getElementById('foto-form').style.borderColor = 'red'
-        alert('A foto do produto não foi informada!')
-    } else {
-        document.getElementById('foto-form').style.borderColor = ''
-    }
-
-    //checagem para vericar se pode as informações sao validas para salvar no local storage
-
-    if (nome !== "" && categoria !== "" && fabricante !== "" && preco !== "" && quantidade !== "" && foto !== "") {
-
-        const produto = {
-            id: Date.now(),
-            nome: nome,
-            categoria: categoria,
-            fabricante: fabricante,
-            preco: preco,
-            quantidade: quantidade,
-            foto: foto
-        }
-
-        let lista = JSON.parse(localStorage.getItem('produtos')) || []
-
-        //se tem o id, edita o produto. se nao, cadastra um novo.
-        if (id !== "") {
-            const posicaoProdutoNoArray = lista.findIndex(produto => produto.id === parseInt(id))
-            lista[posicaoProdutoNoArray] = produto
-            document.getElementById('id-form').value = ""   
-        } else {
-            lista.push(produto)
-        }
-
-        document.getElementById('img-form').setAttribute('src', '/image/img-med-generico.png')
-
-        localStorage.setItem('produtos', JSON.stringify(lista))
-    }
-
-    document.getElementById('Formulario').reset();
-
-    mostrarProdutos();
+  mostrarProdutos();
 }
 
-
-document.getElementById('Formulario').addEventListener('submit', cadastro_formulario)
-
-
+document.getElementById("Formulario").addEventListener("submit", cadastroFormulario);
 
 //Essa função vai exibir os produtos na tela, que foi pego no localstorage acima
 
 function mostrarProdutos() {
+  let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
 
-    let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+  //Foi criado no html a Div lista-produtos que vai listar os produtos
+  const areaProdutos = document.getElementById("lista-produtos");
 
-   //Foi criado no html a Div lista-produtos que vai listar os produtos
-    const areaProdutos = document.getElementById('lista-produtos');
+  areaProdutos.innerHTML = "";
 
-
-    areaProdutos.innerHTML = '';
-
-
-    if (produtos.length === 0) {
-        areaProdutos.innerHTML = '<p>Nenhum produto cadastrado.</p>';
-    } else {
-
-        // esse forEach irá criar para cada produto um cartão 
-        produtos.forEach(function(produto) {
-
-            let cartaoProduto = `
+  if (produtos.length === 0) {
+    areaProdutos.innerHTML = "<p>Nenhum produto cadastrado.</p>";
+  } else {
+    // esse forEach irá criar para cada produto um cartão
+    produtos.forEach(function (produto) {
+      let cartaoProduto = `
                 <div class="produto-card">
                     <img src="${produto.foto}" alt="${produto.nome}" width="100px" height="100px">
                     <h3>${produto.nome}</h3>
                     <p><span class="label">Categoria:</span> ${produto.categoria}</p>
-                    <p><span class="label"> Fabricante:</span> ${produto.fabricante}</p>
+                    <p><span class="label">Fabricante:</span> ${produto.fabricante}</p>
                     <p><span class="label">Preço:</span> R$ ${produto.preco}</p>
                     <p><span class="label">Quantidade:</span> ${produto.quantidade}</p>
                     <div class="btn-container">
@@ -134,135 +122,112 @@ function mostrarProdutos() {
                 </div>
             `;
 
-
-            areaProdutos.innerHTML += cartaoProduto;
-        });
-    }
+      areaProdutos.innerHTML += cartaoProduto;
+    });
+  }
 }
 
-document.addEventListener('DOMContentLoaded', mostrarProdutos);
-
-
-
+document.addEventListener("DOMContentLoaded", mostrarProdutos);
 
 // carregando produdosIniciais vindos do json
 
-function carregarProdutosIniciais () {
+function carregarProdutosIniciais() {
+  let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
 
-    let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
-
-    // verifica se a lista está vazia para preencher os produtos iniciais no localStorage
-    if (produtos.length === 0) {
-        localStorage.setItem('produtos', JSON.stringify(produtosIniciais))
-    } 
-    
+  // verifica se a lista está vazia para preencher os produtos iniciais no localStorage
+  if (produtos.length === 0) {
+    localStorage.setItem("produtos", JSON.stringify(produtosIniciais));
+  }
 }
 
-carregarProdutosIniciais()
+carregarProdutosIniciais();
 
 document.getElementById("foto-form").addEventListener("input", function (event) {
-    document.getElementById('img-form').setAttribute('src', this.value)
-})
+  document.getElementById("img-form").setAttribute("src", this.value);
+});
 
-
-//Edição dos produtos
+// Edição dos produtos
 
 function editarProduto(id) {
-    const produtos = JSON.parse(localStorage.getItem('produtos')) || []
-    const produtoEditar = produtos.find(produto => produto.id === id)
-    
-    document.getElementById('nome-form').value = produtoEditar.nome
-    document.getElementById('categoria-form').value = produtoEditar.categoria
-    document.getElementById('fabricante-form').value = produtoEditar.fabricante
-    document.getElementById('preco-form').value = produtoEditar.preco
-    document.getElementById('estoque-form').value = produtoEditar.quantidade
-    document.getElementById('foto-form').value = produtoEditar.foto
+  const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+  const produtoEditar = produtos.find((produto) => produto.id === id);
 
-    document.getElementById('img-form').setAttribute('src', produtoEditar.foto)
+  document.getElementById("nome-form").value = produtoEditar.nome;
+  document.getElementById("categoria-form").value = produtoEditar.categoria;
+  document.getElementById("fabricante-form").value = produtoEditar.fabricante;
+  document.getElementById("preco-form").value = produtoEditar.preco;
+  document.getElementById("estoque-form").value = produtoEditar.quantidade;
+  document.getElementById("foto-form").value = produtoEditar.foto;
 
-    document.getElementById('id-form').value = produtoEditar.id
+  document.getElementById("img-form").setAttribute("src", produtoEditar.foto);
 
+  document.getElementById("id-form").value = produtoEditar.id;
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Essa opção faz a rolagem suave
+  });
 }
 
-window.editarProduto = editarProduto
+window.editarProduto = editarProduto;
 
 // Excluir um produto do LocalStorage
-
 function excluirProduto(id) {
+  let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
 
-    let produtos = JSON.parse(localStorage.getItem('produtos')) || []
+  produtos = produtos.filter((produto) => produto.id !== id);
 
-    produtos = produtos.filter(produto => produto.id !== id)
+  localStorage.setItem("produtos", JSON.stringify(produtos));
 
-    localStorage.setItem('produtos', JSON.stringify(produtos))
-
-    mostrarProdutos()
-
+  mostrarProdutos();
 }
 
 window.excluirProduto = excluirProduto;
 
-
-// Resumo que essa função faz, ele cria um localstorage temporário para colocar os produtos filtratos e exibe em tela, quando clicar em limpar ele exclui o temporário e dar um refresh na tela.
-
+// Função cria um localstorage temporário para colocar os produtos filtratos e exibe em tela, quando clicar em limpar ele exclui o temporário e faz um refresh na tela.
 function filtrarProdutos() {
-    const nomeFiltro = document.getElementById('filtro-nome').value.toLowerCase();
-    const fabricanteFiltro = document.getElementById('filtro-fabricante').value.toLowerCase();
+  const nomeFiltro = document.getElementById("filtro-nome").value.toLowerCase();
+  const fabricanteFiltro = document.getElementById("filtro-fabricante").value.toLowerCase();
+  const categoriaFiltro = document.getElementById("filtro-categoria").value.toLowerCase();
 
-    let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+  const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
 
-    let produtosFiltrados = produtos.filter(produto => {
-        const nomeInclui = produto.nome.toLowerCase().includes(nomeFiltro);
-        const fabricanteInclui = produto.fabricante.toLowerCase().includes(fabricanteFiltro);
+  const produtosFiltrados = produtos.filter((produto) => {
+    const nomeInclui = produto.nome.toLowerCase().includes(nomeFiltro);
+    const fabricanteInclui = produto.fabricante.toLowerCase().includes(fabricanteFiltro);
+    const categoriaInclui = produto.categoria.toLowerCase().includes(categoriaFiltro);
 
-        // Combinações para o filtro funcionar
-        if (nomeFiltro && fabricanteFiltro) {
-            return nomeInclui && fabricanteInclui;
-        } else if (nomeFiltro) {
-            return nomeInclui;
-        } else if (fabricanteFiltro) {
-            return fabricanteInclui;
-        } else {
-            return true; 
-        }
-     
-    });
+    return (!nomeFiltro || nomeInclui) && (!fabricanteFiltro || fabricanteInclui) && (!categoriaFiltro || categoriaInclui);
+  });
 
-    
-    localStorage.setItem('produtosFiltrados', JSON.stringify(produtosFiltrados));
+  localStorage.setItem("produtosFiltrados", JSON.stringify(produtosFiltrados));
 
-    
-    mostrarProdutosFiltrados();
+  mostrarProdutosFiltrados();
 }
-
 
 function mostrarProdutosFiltrados() {
-    let produtosFiltrados = JSON.parse(localStorage.getItem('produtosFiltrados')) || [];
-    
-    
-    localStorage.setItem('produtos', JSON.stringify(produtosFiltrados));
-    
-    
-    mostrarProdutos();
-    
-    
-    localStorage.setItem('produtos', JSON.stringify(produtosFiltrados));
+  let produtosFiltrados = JSON.parse(localStorage.getItem("produtosFiltrados")) || [];
+
+  localStorage.setItem("produtos", JSON.stringify(produtosFiltrados));
+
+  mostrarProdutos();
+
+  localStorage.setItem("produtos", JSON.stringify(produtosFiltrados));
 }
 
+document.getElementById("btn-pesquisar").addEventListener("click", filtrarProdutos);
 
-document.getElementById('btn-pesquisar').addEventListener('click', filtrarProdutos);
-document.getElementById('btn-limpar').addEventListener('click', function() {
-    document.getElementById('filtro-nome').value = '';
-    document.getElementById('filtro-fabricante').value = '';
-   
-    // Remover o local storage criado temporaio 
-    localStorage.removeItem('produtosFiltrados');
-    
-    mostrarProdutos(); 
-    // Restaura os produtos originais no localStorage
-    localStorage.setItem('produtos', JSON.stringify(produtosIniciais));
+document.getElementById("btn-limpar").addEventListener("click", function () {
+  document.getElementById("filtro-nome").value = "";
+  document.getElementById("filtro-fabricante").value = "";
 
-    // Refresh na pagina
-    location.reload();
+  // Remover o local storage criado temporaio
+  localStorage.removeItem("produtosFiltrados");
+
+  mostrarProdutos();
+  // Restaura os produtos originais no localStorage
+  localStorage.setItem("produtos", JSON.stringify(produtosIniciais));
+
+  // Refresh na pagina
+  location.reload();
 });
-
